fbd41d13512b64567231417d434481ee89a0cb70
shubhamlijade55-arch <shubhamlijade55@gmail.com>
2026-08-25 03:23:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello, World! This web application is running successfully on Google App Engine PaaS.</h1>"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
